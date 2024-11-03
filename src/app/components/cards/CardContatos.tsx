c6bacda073fe254fe5a1348782a51c64a996c4b3
Edson Santos <129888422+Edsonsantos0840@users.noreply.github.com>
2024-11-03 20:47:30 -0300

    ajustes  na responsividade dos icones contatos.

--- src/app/components/cards/CardContatos.tsx
+++ src/app/components/cards/CardContatos.tsx
@@ -10,13 +10,13 @@ export default function CardContatos(): React.JSX.Element {
     className="alinha4 md:flex body2 w-full pt-10 md:pt-14"
   >
     {/* Sessão de Contatos */}
-    <h2 className="text-[var(--corPrincipalEnd)] my-1 md:my-3 text-[1.1rem] md:text-[1.6rem] lg:text-[1.9rem]">
+    <h2 className="text-[var(--corPrincipalEnd)] my-1 md:my-3 text-[1.4rem] md:text-[1.6rem] lg:text-[1.9rem]">
       Meus contatos
     </h2>
     <h3 className="text-[var(--corPrincipalEnd)] my-1 md:my-3 text-[1.4rem] md:text-[1.5rem] lg:text-[1.8rem] ">
       Quer me conhecer melhor?
     </h3>
-    <p className="text-[var(--corPrincipalEnd)] mt-[-5px] lg:my-1 text-[1rem] md:text-[1.4rem] lg:text-[1.3rem]">
+    <p className="text-[var(--corPrincipalEnd)] mt-[-5px] lg:my-1 text-[1.2rem] md:text-[1.4rem] lg:text-[1.3rem]">
       Entre em contato comigo.
     </p>
     <div className="alinha3 gap-4 md:gap-6 my-2 md:my-8">
