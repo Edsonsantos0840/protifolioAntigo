4e853854a394cf461e3b127cbcef2f31e521c041
Edson Santos <129888422+Edsonsantos0840@users.noreply.github.com>
2024-11-03 21:27:35 -0300

    ajustes no botão.

--- src/app/components/botões/BotãoInvertido.tsx
+++ src/app/components/botões/BotãoInvertido.tsx
@@ -6,7 +6,7 @@ interface PropsBotao {
 
 export default function BotaoInvertido(props: PropsBotao) {
   return (
-    <button className={'my-2 py-2  text-sm w-full md:text-md rounded-md bg-[var(--base)] text-[var(--corPrincipal)]  hover:scale-105 shadow-md ease-in duration-300'} >
+    <button className={'my-2 py-2  text-sm w-full md:text-lg rounded-md bg-[var(--base)] text-[var(--corPrincipal)]  hover:scale-105 shadow-md ease-in duration-300'} >
         {props.texto}
     </button>
   )
